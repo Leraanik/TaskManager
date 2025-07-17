84381df59a2477009b1fa24f88036d7964e29aed
Leraanik <valeriaanistratenko27@gmail.com>
2025-07-17 23:53:38 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/TaskManager">
+    <BrowserRouter  basename="/TaskManager2/dist">
       <App />
     </BrowserRouter>
   </React.StrictMode>
