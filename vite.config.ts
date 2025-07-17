caa972165df52c6a983bb0d5f45264320dbd8bf9
Leraanik <valeriaanistratenko27@gmail.com>
2025-07-17 23:57:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TaskManager2/dist/',
+  base: '/TaskManager/',
   plugins: [react()],
 })
